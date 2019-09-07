e13e034130da5e14f4964263ae469b9137ac8eb8
yasas <yasas@192.168.1.149>
2019-09-07 08:54:24 -0500

    First comit

---
+++ FirstProj/src/main/java/com/example/FirstProj/FirstProjApplication.java
@@ -0,0 +1,18 @@
+package com.example.FirstProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjApplication.class, args);
+	
+	 
+	System.out.println("welcome to boot"  );
+	
+	}
+
+}  
+  
